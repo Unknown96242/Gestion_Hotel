d9a1e462ea6738d7b3406304e1db1c0d5edf52b9
ray tchikaya <raytchikaya2004@gmail.com>
2025-05-29 11:39:35 +0000

    modif

---
+++ js/affichageRoles.js
@@ -0,0 +1,11 @@
+// import { recupererDonnee, redirection, estEmailValide, afficherErreurSimple  } from './utils.js';
+
+// const roles = document.getElementById("roles")
+// recupererDonnee('http://localhost:3000/roles')
+// .then( data => {        
+//     for(const titre of data){
+//         for (const element of titre.permissions) {
+//             roles.innerHTML +=`<li>${element}</li>`
+//         }    
+//     }
+// })
